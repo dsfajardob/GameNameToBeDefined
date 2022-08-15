9808febc300ac87375893d5260d5c456143159c5
dsfajardob <dsfajardob@gmail.com>
2022-08-15 11:26:32 -0500

    basic settings

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello world!")
+from json.tool import main
+import pygame, sys
+from settings import * 
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WITDTH, SCREEN_HEIGHT)) 
+        self.clock = pygame.time.Clock()
+    
+    def run(self) -> None:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                     pygame.quit()
+                     sys.exit()
+            
+            dt = self.clock.tick() / 1000
+            pygame.display.update()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
+
---
+++ settings.py
@@ -0,0 +1,4 @@
+from pygame.math import Vector2
+
+SCREEN_WITDTH = 1080
+SCREEN_HEIGHT = 720
